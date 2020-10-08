02b7054afaf6fadb18f2addeb5eee02cb02a3214
KiiDii <kiidii.aniname@gmail.com>
2020-10-08 17:07:34 +0300

    Changed project structure

--- api-config/server-ping.js
+++ api-config/server-ping.js
@@ -1,6 +1,6 @@
 const config = {
 	description: 'Handler showing that the server is working fine',
-	handler:     `lambda/server/ping.handler`,
+	handler:     `lambda/server-ping.handler`,
 	events:      [
 		{ http: { method: 'ANY', path: '/'            } },
 		{ http: { method: 'ANY', path: '/server/ping' } },
